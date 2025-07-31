b2630c46d69288dbc71bd8564dd789c9ae153d24
lautaroBulla <lautaro.portillo.bulla@gmail.com>
2025-07-31 13:30:00 -0300

    i18n

---
+++ src/i18n/en.ts
@@ -0,0 +1,18 @@
+export default {
+  themeOptions: {
+    label: "Theme:",
+    dark: "Dark",
+    light: "Light"
+  },
+  languageOptions: {
+    label: "Language:",
+    es: "Spanish",
+    en: "English"
+  },
+  nav: {
+    about: "About Me",
+    experience: "Experience",
+    projects: "Projects",
+    technologies: "Technologies"
+  },
+}
---
+++ src/i18n/es.ts
@@ -0,0 +1,18 @@
+export default {
+  themeOptions: {
+    label: "Tema:",
+    dark: "Oscuro",
+    light: "Claro"
+  },
+  languageOptions: {
+    label: "Idioma:",
+    es: "Español",
+    en: "Inglés"
+  },
+  nav: {
+    about: "Sobre mí",
+    experience: "Experiencia",
+    projects: "Proyectos",
+    technologies: "Tecnologías",
+  },
+}

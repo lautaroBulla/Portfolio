6b74c4242c6f9ff63cd4def5fba26fb0630a55e0
lautaroBulla <lautaro.portillo.bulla@gmail.com>
2025-08-01 14:57:41 -0300

    blur main

--- src/scripts/asideMenu.js
+++ src/scripts/asideMenu.js
@@ -2,15 +2,18 @@ const arrowRightButton = document.querySelector('#arrow-right-button');
 const arrowRightSvg = document.querySelector('#arrow-right-svg');
 const arrowLeftSvg = document.querySelector('#arrow-left-svg');
 const asideMenu = document.querySelector('aside');
+const mainContent = document.querySelector('main');
 
 arrowRightSvg.addEventListener('click', () => {
   asideMenu.classList.add('open');
+  mainContent.classList.add('filter');
   arrowRightSvg.classList.add('hidden');
   arrowLeftSvg.classList.remove('hidden');
 });
 
 arrowLeftSvg.addEventListener('click', () => {
   asideMenu.classList.remove('open');
+  mainContent.classList.remove('filter');
   arrowLeftSvg.classList.add('hidden');
   arrowRightSvg.classList.remove('hidden');
 });
@@ -30,6 +33,7 @@ mediaQuery.addEventListener('change', (e) => {
     arrowLeftSvg.classList.remove('hidden');
   } else {
     asideMenu.classList.remove('open');
+    mainContent.classList.remove('filter');
     arrowLeftSvg.classList.add('hidden');
     arrowRightSvg.classList.remove('hidden');
   }
